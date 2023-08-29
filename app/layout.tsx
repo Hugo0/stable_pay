044fdafefeadce24fb8cc16a8898d566d8e7916d
Mukul <mukulkadyan202@gmail.com>
2023-08-29 18:25:13 +0530

    updated appDir for pwa functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Pwa from './Pwa'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Pwa />
+      </body>
     </html>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,16 +1,33 @@
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
+const withPWAInit = require("next-pwa");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: isDev,
+  
+  exclude: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+      if (
+        asset.name.startsWith("server/") ||
+        asset.name.match(/^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/)
+      ) {
+        return true;
+      }
+      if (isDev && !asset.name.startsWith("static/runtime/")) {
+        return true;
+      }
+      return false;
+    }
+  ],
 });
 
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-reactStrictMode: true,
-})
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
