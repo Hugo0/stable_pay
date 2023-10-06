30a5da781767c2f7ccd38a0f2a94b362dd4fbd0b
Mukul <mukulkadyan202@gmail.com>
2023-10-06 23:13:42 +0530

    wallet heading sorted

--- components/WalletComponent.tsx
+++ components/WalletComponent.tsx
@@ -84,9 +84,9 @@ const WalletComponent = (props:Props) => {
     },[props.baseCurrency]);
   
     return (
-        <div className="flex-center flex-col md:flex-row gap-x-2 gap-y-3 heading3">
-            <p className="text-gradient_blue-purple text-xl font-bold">Wallet balance:</p>
-            <p className="text-gradient_purple-blue text-xl font-bold">{walletBalance}</p>
+        <div className="flex-center flex-col md:flex-row gap-x-2 gap-y-3">
+            <p className="text-gradient_blue-purple text-xl font-bold heading3">Wallet balance:</p>
+            <p className="text-gradient_purple-blue text-xl font-bold heading3">{walletBalance}</p>
         </div>
     );
 };
